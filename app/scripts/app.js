e41d1d892de9b29478ac68e1d13911523f660cd7
Saliha Butt <saliha.butt@tkxel.com>
2015-10-26 19:41:08 +0500

    my cmmit

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -8,7 +8,7 @@
  *
  * Main module of the application.
  */
-angular
+var app = angular
   .module('sureAuditAdminApp', [
     'ngAnimate',
     'ngCookies',
@@ -18,15 +18,38 @@ angular
     'ngTouch',
     'ui.router'
   ])
+  .constant('configurations',{
+	  'custkey':'propelics',
+	  'appkey':'sureaudit',
+	  'devicekey':'tkxel',
+	  'grant_type':'password',
+	  'identity','https://identity-dev.propelics.com',
+	  'serviceBase','https://sureaudit-dev.propelics.com',
+	  'contentType','application/x-www-form-urlencoded',
+	  'acceptType','application/json'
+  })
   .config(function($stateProvider, $urlRouterProvider) {
   //
   // For any unmatched url, redirect to /state1
   $urlRouterProvider.otherwise("/");
-    //
+  
     // Setting up the states
     $stateProvider
       .state('login', {
         url: "/",
         templateUrl: "views/login.html"
       });
-  });
+  })
+  .run(['$rootScope', '$injector', function($rootScope,$injector){ 
+	  	$injector.get("$http").defaults.transformRequest = function(data, headersGetter){
+	  	 debugger;
+		  if ($rootScope.oauth) {
+			  headersGetter()['Authorization'] = "Bearer "+$rootScope.oauth.access_token; 
+		  }
+		  if (data) { 
+			  return angular.toJson(data);
+		  }
+		}; 		 
+}); 
+		  
+		  
---
+++ app/scripts/services/authService.js
@@ -0,0 +1,37 @@
+'use strict';
+
+app.factory('authService', function ($http, $q, $rootScope,configurations) {
+	
+	var autService = configurations.identity;
+	var authServiceFactory = {};
+	 
+    var _authentication = {
+        isAuth: false,
+        userName : ""
+    };
+    
+
+    var _login = function (loginData) {
+ 
+        var data = "grant_type=password&username=" + loginData.userName + "&password=" + loginData.password;
+ 
+        var deferred = $q.defer();
+ 
+        $http.post(serviceBase + 'token', data, { headers: { 'Content-Type': 'application/x-www-form-urlencoded' } }).success(function (response) {
+ 
+            localStorageService.set('authorizationData', { token: response.access_token, userName: loginData.userName });
+ 
+            _authentication.isAuth = true;
+            _authentication.userName = loginData.userName;
+ 
+            deferred.resolve(response);
+ 
+        }).error(function (err, status) {
+            _logOut();
+            deferred.reject(err);
+        });
+ 
+        return deferred.promise;
+ 
+    };
+});
