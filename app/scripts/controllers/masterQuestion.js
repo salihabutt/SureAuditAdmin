e26df57f6f70a298aaf0ecad68e132ffb0e657d0
bilal qureshi <bilal.qureshi@tkxel.com>
2015-11-02 16:09:21 +0500

    master question integration

---
+++ app/scripts/controllers/masterQuestion.js
@@ -0,0 +1,21 @@
+'use strict';
+
+angular.module('sureAuditAdminApp')
+  .controller('MasterQuestion', function ($scope, masterQuestionService) {
+  
+  	var self = this,
+  	
+		init = function () {
+			masterQuestionService.getData().then(function (response){
+				self.data = {};
+				self.data = response;
+				console.log(self.data)
+
+			}, function (err){
+				console.log(err);
+			});
+		};
+
+		init();
+
+  });
---
+++ app/scripts/services/masterQuestionService.js
@@ -0,0 +1,26 @@
+"use strict";
+
+angular.module('sureAuditAdminApp')
+	.factory('masterQuestionService', function ($http, $q, configurations) {
+		
+		var masterQuestionFactory = {};
+		var _getData = function(){
+			var deferred = $q.defer();
+			var request = {
+				method: 'GET',
+				url: configurations.sureAudit + configurations.serviceBase + 'MasterQuestionSummaries'
+			}
+
+	    	$http(request).success( function(response){
+	    		deferred.resolve(response);
+		  	}).error(function(err,status){
+		  		deferred.reject(err);
+		  	});
+			
+
+		  	return deferred.promise;
+		};
+
+		masterQuestionFactory.getData = _getData;
+		return masterQuestionFactory;
+	});
