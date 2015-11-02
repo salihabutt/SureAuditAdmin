fa44696e9aaf744da8dc20d5a362fa0768dae060
Saliha Butt <saliha.butt@tkxel.com>
2015-11-02 18:15:29 +0500

    updated

--- app/scripts/controllers/login.js
+++ app/scripts/controllers/login.js
@@ -2,21 +2,37 @@
 
 angular.module('sureAuditAdminApp')
   .controller('LoginCtrl', function ($scope, $state, authService) {
-    var self = this,
-    init = function () {
-    	self.loginData = {};
+	var scope = $scope;
+    var self = this;
+    self.disableLoginBtn = true;
+    scope.loginData = {
+    		userName: '',
+    		password: '',
+    		customerId:'',
+    		rememberMe: false
     };
-    
+    self.loginDataCopy = angular.copy(scope.loginData);
     self.isError = false;
     self.login = function () {	
     	self.isError = false;
-		authService.login(self.loginData).then(function (response){			
+		authService.login(scope.loginData).then(function (response){			
 			$state.go('home');
 		}, function (err){
 			self.isError = true;
-			self.loginData = {};
+			$scope.loginData = {};
 		});
     };
     
-    init();
+    scope.$watch('loginData', function(newVal, oldVal){
+    	scope.loginData.userName = newVal.userName===undefined? '' : newVal.userName;
+    	scope.loginData.password = newVal.password===undefined? '' : newVal.password;
+    	scope.loginData.customerId = newVal.customerId===undefined? '' : newVal.customerId;
+    	if (!angular.equals(self.loginDataCopy,scope.loginData)) {
+    		self.disableLoginBtn = false;
+    	}else {
+    		self.disableLoginBtn = true;
+    	}
+    },true);
+    
+    
   });
