c8d3ef6860a7030783f084f552dc0e34f882190e
Saliha Butt <saliha.butt@tkxel.com>
2015-11-06 12:46:38 +0500

    edit is completed

--- app/scripts/controllers/masterQuestion.js
+++ app/scripts/controllers/masterQuestion.js
@@ -27,18 +27,32 @@ angular.module('sureAuditAdminApp')
 			self.predicate = predicate;
 		};
 	  
-	  self.addEditMasterQuestion = function () {
+	  self.addEditMasterQuestion = function (question, action) {
+		  var item = {};
+		  item.action = action;
+		  item.question = question;
 		 $uibModal.open({
 			  animation: true,
 			  templateUrl: 'addEditMasterQuestion.html',
 			  controller: 'addEditMasterQuestionCtrl',
 			  windowClass: 'questions-modal',
 			  controllerAs: 'qmModal',
-		  }).result.then(function(data) {
-			  data.Key = "key" + self.data.Data.length +1;
-			  masterQuestionService.saveQuestion(data).then(function (response){
-				  console.log(response);
-				  self.getQuestions();
+			  resolve: {
+				  item: function () {
+					  return item;
+				  }
+			  } 
+		  }).result.then(function(mq) {
+			  mq.Key = action === 'ADD'? 'key' + self.data.Data.length +1 : mq.Key;
+			  masterQuestionService.saveUpdateQuestion(mq,action).then(function (response){
+				  debugger;
+				  if(action === 'ADD'){
+					  question.Id = response.Id;
+					  self.data.Data.push(question);
+				  }else{
+					  	var index = self.data.Data.map(function(x) {return x.Id; }).indexOf(mq.Id);
+						self.data.Data[index] = mq;
+				  }
 				}, function (){
 					//TODO ERROR block
 				});
@@ -60,21 +74,21 @@ angular.module('sureAuditAdminApp')
 				url: configurations.sureAudit + configurations.serviceBase + 'MasterQuestions/'+ id 
 			};
 			$http(request).success( function (response) {
-	    		if(response.Error == null){
+	    		if(response.Error === null){
 					//to do : check the performance issue plus cross browser compatibility
 					//var idx = _.chain(self.data.Data).pluck(Id).indexOf(response.Id).value();
 					var index = self.data.Data.map(function(x) {return x.Id; }).indexOf(id);
 					self.data.Data[index].Status = "Deleted";
 				}
 		  	}).error(function(err){
-		  		console.log(err)
+		  		console.log(err);
 		  	});
 		  });
 	  };
 	  
 	  init();
   })
-  .controller('addEditMasterQuestionCtrl', function ($uibModalInstance, lookupService, masterQuestionModel) {
+  .controller('addEditMasterQuestionCtrl', function ($uibModalInstance, lookupService, masterQuestionModel, item) {
 
 	  	var self = this,
 	  	init = function () {
@@ -82,13 +96,32 @@ angular.module('sureAuditAdminApp')
 	  	 	self.masterQuestionModel = angular.copy(masterQuestionModel);
 		  	self.masterQuestionClone = angular.copy(self.masterQuestionModel);
 			self.questionTypes = lookupService.questionTypesObj();
-			self.selectedOption = '';
 			self.disableSaveBtn = true;
+			self.subject = "Add New Master Question";
+			if(item.action === 'EDIT') {
+				self.initEditModal();
+			}
 	  	};
 	  	
-	 
+	  	self.initEditModal = function () {
+	  		self.subject = "Edit Master Question";
+			self.masterQuestionModel = angular.copy(item.question);
+		  	self.masterQuestionClone = angular.copy(self.masterQuestionModel);
+		  	if(item.question.AllowableValues.length>0){
+		  		self.optionItems=[];
+		  	}
+		  	for(var i=0; i< self.masterQuestionModel.AllowableValues.length; i++) {
+		  		var temp = {};
+		  		temp.id = parseInt(self.masterQuestionModel.AllowableValues[i].Key);
+		  		temp.label = self.masterQuestionModel.AllowableValues[i].Label;
+		  		temp.value = self.masterQuestionModel.AllowableValues[i].Value;
+		  		self.optionItems.push(temp);
+		  	}
+	  	}
 	  	
 	  	self.ok = function () {
+	  		self.optionItems.sort(function(a, b) { return a.id - b.id; });
+	  		self.masterQuestionModel.AllowableValues = [];
 	  		for(var i=0; i< self.optionItems.length; i++) {
 	  			var obj = {};
 	  			obj.Key = self.optionItems[i].id;
@@ -109,7 +142,7 @@ angular.module('sureAuditAdminApp')
 		};
 		
 		self.updateModel = function () {
-			if ((self.masterQuestionClone.Text !== self.masterQuestionModel.Text) && (self.masterQuestionClone.TypeKey !== self.masterQuestionModel.TypeKey)) {
+			if ((self.masterQuestionClone.Text !== self.masterQuestionModel.Text) && (self.masterQuestionModel.TypeKey !== '')) {
 	    		self.disableSaveBtn = false;
 	    	}else {
 	    		self.disableSaveBtn = true;
--- app/scripts/services/masterQuestionService.js
+++ app/scripts/services/masterQuestionService.js
@@ -21,15 +21,35 @@ angular.module('sureAuditAdminApp')
 		  	return deferred.promise;
 		};
 		
-		var _saveQuestion = function (data) {
-			var deferred = $q.defer();
+		var _saveQuestion = function (question) {
 			var request = {
 				method: 'POST',
 				url: configurations.sureAudit + configurations.serviceBase + 'MasterQuestions',
-				data: JSON.stringify(data)
+				data: JSON.stringify(question)
 			};
-
-	    	$http(request).success( function (response) {
+		  	return request;			
+		};
+		
+		var _updateQuestion = function (question) {
+			var request = {
+				method: 'PUT',
+				url: configurations.sureAudit + configurations.serviceBase + 'MasterQuestions/' + question.Id,
+				data: JSON.stringify(question)
+			};
+	    	return request;			
+		};
+		
+		var _saveUpdateQuestion = function (question,action) {
+			var deferred = $q.defer();
+			var request = {};
+			if(action === 'ADD') {
+				request = _saveQuestion(question);
+				
+			}else {
+				request = _updateQuestion(question);
+			}
+			
+			$http(request).success( function (response) {
 	    		deferred.resolve(response);
 		  	}).error(function(err){
 		  		deferred.reject(err);
@@ -37,10 +57,11 @@ angular.module('sureAuditAdminApp')
 			
 
 		  	return deferred.promise;
-			
 		};
 
 		masterQuestionFactory.getData = _getData;
 		masterQuestionFactory.saveQuestion = _saveQuestion;
+		masterQuestionFactory.updateQuestion = _updateQuestion;
+		masterQuestionFactory.saveUpdateQuestion = _saveUpdateQuestion;
 		return masterQuestionFactory;
 	});
