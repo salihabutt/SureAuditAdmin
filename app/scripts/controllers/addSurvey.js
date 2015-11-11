5810a476742de3e21e3727c0b29fcd83b24fdde2
Saliha Butt <saliha.butt@tkxel.com>
2015-11-11 09:48:39 +0500

    add survey

--- app/scripts/controllers/addSurvey.js
+++ app/scripts/controllers/addSurvey.js
@@ -1,7 +1,7 @@
 'use strict'
 
 angular.module('sureAuditAdminApp')
-.controller('AddEditSurveyCtrl', function ($stateParams, surveyModel, utilityService) {
+.controller('AddEditSurveyCtrl', function ($stateParams, $uibModal, surveyModel, utilityService) {
 	var self = this,
 	init = function () {
 		self.auditDefinition = {};
@@ -11,6 +11,18 @@ angular.module('sureAuditAdminApp')
 			self.auditDefinition.Key = utilityService.guid;
 		}
 	};
-	
+	self.addQuestion = function () {
+		$uibModal.open({
+			animation: false,
+			templateUrl: 'addSurveyQuestion.html',
+			controller: 'AddSurveyQuestionCtrl',
+			windowClass: 'survey-questions-modal',
+			controllerAs: 'mqModal'
+		})
+	};
 	init();
+})
+.controller('AddSurveyQuestionCtrl', function (){
+	
+	
 });
