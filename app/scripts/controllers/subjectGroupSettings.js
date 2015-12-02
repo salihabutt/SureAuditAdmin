d9245e858d04adc7ee00ee1a4e51df990294ebe9
Saliha Butt <saliha.butt@tkxel.com>
2015-12-02 18:35:38 +0500

    subject group updated

--- app/scripts/controllers/subjectGroupSettings.js
+++ app/scripts/controllers/subjectGroupSettings.js
@@ -5,18 +5,25 @@ angular.module('sureAuditAdminApp')
 	var self = this,
 	init = function () {
 		var id = $stateParams.id;
+		self.initSubjectAttributes();
+		self.initProfileAttributes();
+		self.types = ['Email','Phone','Other'];
 		SubjectGroupService.getSubjectGroup(id).then(function (response){
 			self.subgrpSettings = response;
 			self.subgrpSettings.TouchInfo.ModifiedDate = new Date();
+			if(self.subgrpSettings.SortOptions != null){
+				self.popuplateSubjectAtt();
+			}
 		});
-		self.initSubjectAttributes();
+	
+		
 		
 	};
 	
 	self.initSubjectAttributes = function () {
 		self.subjectAttList = [];
 		var subjectAtt = {};
-		subjectAtt.label = '';
+		subjectAtt.order = '';
 		subjectAtt.Key = '';
 		subjectAtt.Name = null;
 		subjectAtt.Field = null;
@@ -27,6 +34,33 @@ angular.module('sureAuditAdminApp')
 		self.subjectAttList.push(angular.copy(subjectAtt));
 		self.subjectAttList.push(angular.copy(subjectAtt));
 	};
+	
+	self.popuplateSubjectAtt = function () {
+		for(var i=0;i<self.subgrpSettings.SortOptions.length;i++){
+			var subjectAtt = {};
+			subjectAtt.order = i;
+			subjectAtt.Key = '';
+			subjectAtt.Name = self.subgrpSettings.SortOptions[i].Name;
+			subjectAtt.Field = null;
+			subjectAtt.isDefault = self.subgrpSettings.SortOptions[i].isDefault;
+			subjectAtt.sortOption = false;	
+			self.subjectAttList[i] = subjectAtt;
+		}
+		console.log(self.subjectAttList);
+	}
+	
+	self.initProfileAttributes = function () {
+		self.profileAttList = [];
+		var profileAtt = {};
+		profileAtt.label = '';
+		profileAtt.order = '';
+		profileAtt.key = '';
+		profileAtt.type = null;
+		self.profileAttList.push(angular.copy(profileAtt));
+		self.profileAttList.push(angular.copy(profileAtt));
+		self.profileAttList.push(angular.copy(profileAtt));
+		self.profileAttList.push(angular.copy(profileAtt));
+	}
 	
 	self.checkIsDefault = function (index,event) {
 		if(event.target.checked){
@@ -40,6 +74,10 @@ angular.module('sureAuditAdminApp')
 	
 	self.saveSubGrpSetting = function () {
 		
+	};
+	
+	self.saveProfileAtt = function () {
+		//for(var i=0;i<)
 	};
 	init();
 	
